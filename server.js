7d547289ceacad41598eff75937a773cac568dc0
Becca Wyland <rwyland@uwalumni.com>
2021-05-10 21:28:10 -0500

    connect to server and database

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const mysql = require('mysql2');
+
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
@@ -6,6 +8,19 @@ const app = express();
 // Express middlware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Connect app to MySQL database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username
+        user: 'root',
+        // Your MySQL password
+        password: '!Quill189!',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
 // Handle user requests not supported by the app / Default response for any other request (not found)
 app.use((req, res) => {
